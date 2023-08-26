23dff653f733482649dfabda53cf6ec37e5c69a5
Harsh-Jagtap-04 <harshjagtap0401@gmail.com>
2023-08-26 12:19:50 +0530

    report html

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ReportForm />
+  </div>
+</template>
+
+<script>
+import ReportForm from './components/ReportForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ReportForm,
+  },
+};
+</script>
+
+<style>
+/* Add global styling here */
+</style>
